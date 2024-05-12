7d7581cad28f26b11d6820614672b88fad91802e
MariusS81 <mariusvilaescu@yahoo.com>
2024-05-12 15:38:56 +0300

    ex3 completed

---
+++ ex3/index.js
@@ -0,0 +1,11 @@
+let numbers = [1, -5, 20, -34, 16, 29, 36, -4];
+
+function reversNumber(array) {
+  let newList = [];
+  for (let i = array.length - 1; i >= 0; i--) {
+      newList.push(array[i]);
+  }
+  return newList;
+}
+
+console.log(reversNumber(numbers));
