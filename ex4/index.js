0d8b30c15cbdb5347565636937cecdbec61848a9
MariusS81 <mariusvilaescu@yahoo.com>
2024-05-13 11:07:28 +0300

    test vocale lit mare

--- ex4/index.js
+++ ex4/index.js
@@ -1,9 +1,9 @@
-function numarVocale(șir) {
+function numarVocale(sir) {
        const vocale = șir.match(/[aeiou]/gi);
        if (vocale === null) {
         return 0;
     }
         return vocale.length;
 }
-const input = "aici se gasesc cateva vocale";
-console.log(numarVocale(input)); 
+const count = "Aici se gasesc cateva vOcalE";
+console.log(numarVocale(count)); 
